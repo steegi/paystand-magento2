7c72313a862f8ab2ed5b62927df24189fcd1b625
Frank Steegmans <fsteegmans@paystand.com>
2016-11-22 02:14:01 +0000

    PPL-92: Use Magneto 2 user and billing details in checkout

--- view/frontend/web/js/view/checkout/checkout.js
+++ view/frontend/web/js/view/checkout/checkout.js
@@ -1,6 +1,6 @@
 var checkoutjs_module = 'paystand';
 var use_sandbox = window.checkoutConfig.payment.paystandmagento.use_sandbox;
-if(use_sandbox){
+if (use_sandbox) {
   checkoutjs_module = 'paystand-sandbox';
 }
 
@@ -12,65 +12,97 @@ define([
   'Magento_Checkout/js/model/quote',
   'Magento_Checkout/js/model/error-processor',
   checkoutjs_module
-], function (require, $, Component, quote, errorProcessor, paystand) {
+], function (require, $, iframe, quote, errorProcessor, paystand) {
   'use strict';
 
   /**
    * Load the Paystand checkout
    */
-  var loadPaystandCheckout = function(){
+  var loadPaystandCheckout = function () {
 
     var core_domain = 'paystand.com';
 
     var use_sandbox = window.checkoutConfig.payment.paystandmagento.use_sandbox;
-    if(use_sandbox){
+    if (use_sandbox) {
       core_domain = 'paystand.co';
     }
 
     var publishable_key = window.checkoutConfig.payment.paystandmagento.publishable_key;
-    console.log("publishable key = "+publishable_key);
+    console.log("publishable key = " + publishable_key);
 
-    //var price = window.checkoutConfig.totalsData.base_grand_total.replace(/[^0-9\.]+/g,"");
-    var price = $(".grand.totals .price").html().replace(/[^0-9\.]+/g,"");
+    var price = quote.totals().grand_total.toString();
     var quoteId = quote.getQuoteId();
+    var billing = quote.billingAddress();
 
-    PayStandCheckout.checkoutComplete = function (data) {
+    PayStandCheckout.checkoutComplete = function (data, iframe) {
       console.log("custom checkout complete:", data);
       $(".submit-trigger").click();
     };
     PayStandCheckout.checkoutFailed = function (data) {
       console.log("custom checkout failed:", data);
     };
-    PayStandCheckout.init({
-      "publishableKey": publishable_key,
-      "checkout_domain": "https://checkout."+core_domain+"/v3/",
-      "domain": "https://api."+core_domain,
-      "payment": {
-        "amount": price
-      },
-      "currency": "USD",
-      "paymentMethods": [
-        'echeck',
-        'card'
-      ],
-      "meta": {
-        "source":"magento 2",
-        "quote": quoteId
-      }
-    },null,520);
 
-    // stop observing for mutation events
-    window.observer.disconnect();
+    function initCheckout(countryISO3) {
+      PayStandCheckout.init({
+        "publishableKey": publishable_key,
+        "checkout_domain": "https://checkout." + core_domain + "/v3/",
+        "domain": "https://api." + core_domain,
+        "payment": {
+          "amount": price
+        },
+        "currency": "USD",
+        "paymentMethods": [
+          'echeck',
+          'card'
+        ],
+        "payer": {
+          "name": billing.firstname + ' ' + billing.lastname,
+          "email": quote.guestEmail
+        },
+        "billing": {
+          "street": billing.street[0],
+          "city": billing.city,
+          "postalCode": billing.postcode,
+          "subdivisionCode": billing.regionCode,
+          "countryCode": countryISO3
+        },
+        "meta": {
+          "source": "magento 2",
+          "quote": quoteId
+        }
+      }, null, 520);
+      // stop observing for mutation events
+      window.observer.disconnect();
+    }
+
+    if(billing.countryId) {
+      $.ajax({
+        beforeSend: function(request) {
+          request.setRequestHeader("x-publishable-key", publishable_key);
+        },
+        dataType: "text",
+        contentType: "application/json; charset=utf-8",
+        url: "https://api." + core_domain + "/v3/addresses/countries/iso?code=" + billing.countryId,
+        success: function(data) {
+          initCheckout(JSON.parse(data).iso3);
+        },
+        error: function(error) {
+          console.log('Unable to get ISO3 code from PayStand!');
+        },
+      });
+    } else {
+      initCheckout();
+    }
   };
 
 // create an observer instance
-  window.observer = new MutationObserver(function(mutations) {
-    mutations.forEach(function(mutation) {
+  window.observer = new MutationObserver(function (mutations) {
+    mutations.forEach(function (mutation) {
       console.log(mutation.type);
       for (var i = 0; i < mutation.addedNodes.length; ++i) {
         var item = mutation.addedNodes[i];
         if (typeof item.getElementsByClassName === "function") {
-          if(item.getElementsByClassName('paystand-checkout-form').length > 0){
+          if (item.getElementsByClassName('paystand-checkout-form').length > 0) {
             loadPaystandCheckout();
           }
         }
@@ -79,19 +111,19 @@ define([
   });
 
   // configuration of the observer:
-  var config = { attributes: true, childList: true, characterData: true };
+  var config = {attributes: true, childList: true, characterData: true};
 
   // pass in the target node, as well as the observer options
   var total_timeout = 0;
-  var recursivelyObserve = function(){
-    window.setTimeout(function(){
+  var recursivelyObserve = function () {
+    window.setTimeout(function () {
       total_timeout += 10;
 
-      if(total_timeout < 1000){
+      if (total_timeout < 1000) {
         // select the target node
-        var target = $('.payment-method').parent().get( 0 );
+        var target = $('.payment-method').parent().get(0);
 
-        if(typeof target == "Node"){
+        if (typeof target == "Node") {
           observer.observe(target, config);
         }
         else {
