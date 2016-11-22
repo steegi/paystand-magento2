257dd9bd28261baf92c1cc6dc953f554418050a7
Frank Steegmans <fsteegmans@paystand.com>
2016-11-22 02:10:16 +0000

    PPL-91: PayStand Magento 2 plug-in does not complete checkout when a successful payment is submitted

--- view/frontend/web/js/view/payment/method-renderer/paystandmagento-directpost.js
+++ view/frontend/web/js/view/payment/method-renderer/paystandmagento-directpost.js
@@ -1,41 +1,15 @@
 define(
-    [
-        'require',
-        'jquery',
-        'Magento_Payment/js/view/payment/iframe',
-        'Magento_Checkout/js/model/quote',
-        'Magento_Checkout/js/action/set-payment-information',
-        'Magento_Checkout/js/model/full-screen-loader',
-        'paystand-checkout'
-    ],
-    function (require, $, Component, quote, setPaymentInformationAction, fullScreenLoader, paystandCheckout) {
-        'use strict';
+  [
+    'Magento_Checkout/js/view/payment/default'
+    , 'paystand-checkout'
+  ],
+  function (Component, paystandCheckout) {
+    'use strict';
 
-        return Component.extend({
-            defaults: {
-                template: 'PayStand_PayStandMagento/payment/paystandmagento-directpost'
-            },
-            placeOrderHandler: null,
-
-            setPlaceOrderHandler: function(handler) {
-                this.placeOrderHandler = handler;
-            },
-
-            context: function() {
-                return this;
-            },
-
-            isShowLegend: function() {
-                return true;
-            },
-
-            getCode: function() {
-                return 'paystandmagento_directpost';
-            },
-
-            isActive: function() {
-                return true;
-            }
-        });
-    }
+    return Component.extend({
+      defaults: {
+        template: 'PayStand_PayStandMagento/payment/paystandmagento-directpost'
+      }
+    });
+  }
 );
